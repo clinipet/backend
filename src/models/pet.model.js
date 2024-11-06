71ed960fdeba85d1475cf20c2417a42e844ea24a
Victor Ramon Balchaki <95146779+Balchaki@users.noreply.github.com>
2024-11-06 13:19:17 -0300

    Include schema in pet model

--- src/models/pet.model.js
+++ src/models/pet.model.js
@@ -2,17 +2,17 @@ const db = require('../config/database');
 
 const Pet = {
   getAll: () => {
-    return db.query('SELECT * FROM pets');
+    return db.query('SELECT * FROM clinipet.pets');
   },
   
   getById: (id) => {
-    return db.query('SELECT * FROM pets WHERE id = ', [id]);
+    return db.query('SELECT * FROM clinipet.pets WHERE id = ', [id]);
   },
   
   create: (pet) => {
     const { client_id, name, species, breed, birth_date, weight, gender } = pet;
     return db.query(
-      'INSERT INTO pets (client_id, name, species, breed, birth_date, weight, gender) VALUES (, , , , , , ) RETURNING *',
+      'INSERT INTO clinipet.pets (client_id, name, species, breed, birth_date, weight, gender) VALUES (, , , , , , ) RETURNING *',
       [client_id, name, species, breed, birth_date, weight, gender]
     );
   },
@@ -20,13 +20,13 @@ const Pet = {
   update: (id, pet) => {
     const { name, species, breed, birth_date, weight, gender } = pet;
     return db.query(
-      'UPDATE pets SET name = , species = , breed = , birth_date = , weight = , gender =  WHERE id =  RETURNING *',
+      'UPDATE clinipet.pets SET name = , species = , breed = , birth_date = , weight = , gender =  WHERE id =  RETURNING *',
       [name, species, breed, birth_date, weight, gender, id]
     );
   },
   
   delete: (id) => {
-    return db.query('DELETE FROM pets WHERE id = ', [id]);
+    return db.query('DELETE FROM clinipet.pets WHERE id = ', [id]);
   }
 };
 
